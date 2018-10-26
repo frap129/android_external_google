7ee80b1ad071d6503a4f022bc5206f4013a971e7
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-25 22:51:52 -0700

    Add SystemUIGoogleFactory to allow casting AssistManager
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ src/com/google/android/systemui/SystemUIGoogleFactory.java
@@ -0,0 +1,18 @@
+package com.google.android.systemui;
+
+import android.content.Context;
+import android.util.ArrayMap;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.Dependency.DependencyProvider;
+import com.android.systemui.statusbar.policy.DeviceProvisionedController;
+import com.android.systemui.SystemUIFactory;
+import com.android.systemui.assist.AssistManager;
+
+public class SystemUIGoogleFactory extends SystemUIFactory {
+    public void injectDependencies(ArrayMap<Object, DependencyProvider> providers, Context context) {
+        super.injectDependencies(providers, context);
+        providers.put(AssistManager.class,
+                () -> new AssistManagerGoogle((DeviceProvisionedController) Dependency.get(DeviceProvisionedController.class), context));
+    }
+}
